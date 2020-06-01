0bb428908d05d14b2d93ecb8eb63546557eed653
Yousif Wali <shtykon87@gmail.com>
2020-06-01 15:06:43 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include 'data.php';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script src="script.js"></script>
+    <title>I do not even know</title>
+    <style media="screen">
+      @import 'style.css';
+    </style>
+  </head>
+  <body>
+    <div id="header">
+      <div>
+        Welcome
+      </div>
+      <div>
+        <form>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
